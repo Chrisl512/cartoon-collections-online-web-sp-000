cf0aec1c1d91f87a1a91f206ba8a6456f92e3196
Christopher Luna <chris@illusionlayervfx.com>
2019-03-18 02:22:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,7 +36,7 @@ def find_the_cheese(array_strings)
     if cheese_types.include?(cheese)
       match = cheese
       new_array << match
-      return new_array
+      return new_array.string
     end
   end
 end
